13c0b539af27c9a8173cc1a6c242901beaf00eaf
nino <qweng@stu.xidian.edu.cn>
2018-06-10 03:05:47 +0800

    xxx

---
+++ src/main/java/site/wengqi/concurrency/ConcurrencyApplication.java
@@ -0,0 +1,15 @@
+package site.wengqi.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ConcurrencyApplication.class, args);
+
+
+    }
+}
